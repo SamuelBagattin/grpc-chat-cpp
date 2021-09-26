0bd60f957a2b2917d948458ac6c8fce78e930b43
Samuel Bagattin <42091952+SamuelBagattin@users.noreply.github.com>
2021-09-26 23:11:07 +0200

    Hello world

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # grpc-chat-cpp
+## Building
+```
+cmake -DCMAKE_PREFIX_PATH=$MY_INSTALL_DIR ../..
+make -j
+```
---
+++ server.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+#include "chat.grpc.pb.h"
+#include "chat.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using helloworld::Greeter;
+using helloworld::HelloReply;
+using helloworld::HelloRequest;
+
+
+
+int main(int argc, char** argv) {
+  return 0;
+}
