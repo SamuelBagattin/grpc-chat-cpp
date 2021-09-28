6c808efb6c0f2be48044977d557e619b70cbd358
Samuel Bagattin <42091952+SamuelBagattin@users.noreply.github.com>
2021-09-28 20:22:08 +0200

    WIP

--- server.cc
+++ server.cc
@@ -11,13 +11,47 @@
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
+using grpc::ServerReaderWriter;
 using grpc::Status;
-using helloworld::Greeter;
-using helloworld::HelloReply;
-using helloworld::HelloRequest;
+using grpc_chat::Message;
+using grpc_chat::MessagesService;
 
+class ChatServiceImpl final : public MessagesService::Service
+{
+  Status Chat(ServerContext *context, ServerReaderWriter<Message, Message> *stream) override
+  {
+    std::vector<Message> received_messages;
+    Message message;
+    while (stream->Read(&message))
+    {
+      for (const Message &msg : received_messages)
+      {
+        std::cout << msg.name() << msg.content() << std::endl;
+      }
+      received_messages.push_back(message);
+    }
+    return Status::OK;
+  }
+};
 
+void RunServer()
+{
+  std::string server_address("0.0.0.0:50051");
+  ChatServiceImpl service;
 
-int main(int argc, char** argv) {
+  grpc::EnableDefaultHealthCheckService(true);
+  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+
+  ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+  server->Wait();
+}
+
+int main(int argc, char **argv)
+{
+  RunServer();
   return 0;
 }
